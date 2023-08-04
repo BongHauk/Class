5db6c796c77d6b9697705b21658dd3c30e9765f0
Branden <btwyolo211@gmail.com>
2023-08-04 16:36:45 +0900

    첫번째 커밋

---
+++ study_test/src/study_test01/Study_test01.java
@@ -0,0 +1,13 @@
+package study_test01;
+
+public class Study_test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Hello Git!");
+
+	}
+
+}
