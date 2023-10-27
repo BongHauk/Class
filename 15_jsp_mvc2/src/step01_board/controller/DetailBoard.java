5ee2ce172cf3689c56bf9aeaac4842fec5d355a6
12_web_bhw <12_web_bhw@DESKTOP-RAO49T2>
2023-10-27 14:47:51 +0900

    2023.10.27

---
+++ 15_jsp_mvc2/src/step01_board/controller/DetailBoard.java
@@ -0,0 +1,27 @@
+package step01_board.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import step01_board.dao.BoardDAO;
+import step01_board.dto.BoardDTO;
+
+@WebServlet("/bDetail")
+public class DetailBoard extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		long boardId = Long.parseLong(request.getParameter("boardId"));
+		
+		BoardDTO boardDTO = BoardDAO.getInstance().getBoardDetail(boardId);
+		System.out.println(boardDTO);
+		
+	}
+
+}
